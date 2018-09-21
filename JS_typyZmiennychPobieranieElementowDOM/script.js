cd240d6eb8bd84cf30ae230516462372b8da1f75
Buba Korowski <Jakub.B.Borowski@gmail.com>
2018-09-21 22:40:06 +0200

    grid_zadanieFlexbox_JavaScriptZmiennePobieranieElementu

---
+++ JS_typyZmiennychPobieranieElementowDOM/script.js
@@ -0,0 +1,35 @@
+console.log("script src dziala ok");
+var courseName = "kurs JS na stronie"; // string, czylli ciag znakow
+console.log(typeof courseName);
+var numberOfPeople = 1200; //number
+console.log(typeof numberOfPeople);
+var distance = 2.1; //number
+var female = false; //boolean true/false
+console.log(female, typeof female);
+var onlyDeclared; // undefined jest nadawany przez interpreter
+var activeTask = null; //null piszemy go sami
+var taskArray = []; //tablica
+var myNewFunction = function() {}; //funkcja TO JEST NAJWAŻNIEJSZA SPRAWA W JS. PODSTAWA PRZYSZŁYCH ZAROBKÓW!!!
+var myObj = {}; //obiekt
+console.log(taskArray);
+
+// pobieranie elementów
+var p = document.querySelector(".description");
+var paragraph = document.querySelector("p");
+var text = document.querySelector("p:nth-child(1)");
+var myPage = document.querySelector("body");
+
+var item1 = document.getElementById("p2");
+// okno przeglądarki
+console.log(window);
+
+// skrót do body
+console.log(document.body);
+console.log(document.documentElement);
+
+// skrót do kolekcji zbioru wszystkich obr  azków na stroneie
+console.log(document.images);
+console.log(document.querySelectorAll("img"));
+// czego używać najczęściej
+document.querySelector("element");
+document.querySelectorAll("elementy");
---
+++ JS_zmienneOrazWykrywanieBłędów/script.js
@@ -0,0 +1,17 @@
+console.log("wszystko sie ok importuje z pliku script.js");
+// zmienne var
+var userName = "Jakub";
+var userAge = 18;
+var drivingLicense = true;
+console.log(userAge, userName);
+
+let myAge = 35;
+let profession = "programer";
+const yearOfBirth = "1983";
+
+// let myAge = 33; to jest błąd ponownej deklaracji zmiennej
+// const yearOfBirth = "33"; const to zmienna która jest stała ;p
+// const adress; brak deklaracji wartości zmiennej
+// let ccc; zmienna let została zainicjalizowana ale nie zdefiniowana
+// var ddd; zmienna var została zainicjalizowana ale nie zdefiniowana
+var buzz = "1 b";
