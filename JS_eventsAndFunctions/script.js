70cadfd24ffb1f4f4a03cacca4dc6e0f3c11608a
Buba Korowski <Jakub.B.Borowski@gmail.com>
2018-09-23 01:38:25 +0200

    events&Functions_JS

--- JS_eventsAndFunctions/script.js
+++ JS_eventsAndFunctions/script.js
@@ -1,23 +1,23 @@
 console.log("ok");
 
 const btn = document.querySelector("button");
-// btn.addEventListener("click", function() {
-//   console.log("klik");
-// });
+btn.addEventListener("click", function() {
+  console.log("klik");
+});
 
 // const bdy = document.querySelector("body");
 
-// btn.addEventListener("mouseover", function() {
-//   console.log("najechanie myszką");
-// });
+btn.addEventListener("mouseover", function() {
+  console.log("najechanie myszką");
+});
 
-// btn.addEventListener("mousemove", function() {
-//   console.log("najechanie myszką");
-// });
+btn.addEventListener("mousemove", function() {
+  console.log("najechanie myszką");
+});
 
-// btn.addEventListener("click", function() {
-//   alert("kliknąłeś");
-// });
+btn.addEventListener("click", function() {
+  alert("kliknąłeś");
+});
 
 // const paragraph = document.querySelector("p");
 // btn.addEventListener("click", function() {
@@ -32,4 +32,7 @@ btn.addEventListener("click", function() {
   // clicksNumber++;
   clicksNumber += 2;
   console.log("klkik numer", clicksNumber);
+});
+window.addEventListener("scroll", function() {
+  console.log("skrollujesz o " + window.scrollY + "px");
 });
